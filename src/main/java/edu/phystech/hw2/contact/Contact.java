516ae798a2395a181e38743368d0c43c96a8c656
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-05-25 07:17:59 +0500

    Update Contact.java

--- src/main/java/edu/phystech/hw2/contact/Contact.java
+++ src/main/java/edu/phystech/hw2/contact/Contact.java
@@ -1,15 +1,44 @@
 package edu.phystech.hw2.contact;
 
+import java.util.List;
+import java.util.stream.Stream;
 
-record Contact(String username, String email) {
-    public static final String UNKNOWN_EMAIL = "unknown";
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-    Contact {
-        // здесь должна быть валидация
+
+public class ContactTest {
+
+    @Test
+    public void justWorks() {
+
+        Assertions.assertDoesNotThrow(() -> {
+            Contact first = new Contact("username", "username@gmail.com");
+            Contact second = new Contact("123", "123@gmail.com");
+
+            Contact withoutEmail = new Contact("withoutEmail");
+            Assertions.assertEquals(Contact.UNKNOWN_EMAIL, withoutEmail.email());
+        });
+
     }
 
-    Contact(String username) { this(null, null); }
+    @Test
+    public void validationTest() {
+        var exception =
+                Assertions.assertThrows(InvalidContactFieldException.class, () -> new Contact("   ", "123@gmai.com"));
+        Assertions.assertEquals("username", exception.getFieldName());
+        exception =
+                Assertions.assertThrows(InvalidContactFieldException.class, () -> new Contact("   1", "123@mai.ru"));
+        Assertions.assertEquals("email", exception.getFieldName());
+        exception = Assertions.assertThrows(InvalidContactFieldException.class, () -> new Contact("   ", ""));
+        Assertions.assertEquals("username", exception.getFieldName());
+    }
 
-
-    public int compareTo(Contact o) {return 0;}
+    @Test
+    public void compareTest() {
+        var result = Stream.of(new Contact("AFD"), new Contact("a"), new Contact("zZ")).sorted(Contact::compareTo)
+                .map(Contact::username).toList();
+        Assertions.assertEquals(List.of("a", "zZ", "AFD"), result);
+        Assertions.assertInstanceOf(Comparable.class, new Contact("AFD"));
+    }
 }
